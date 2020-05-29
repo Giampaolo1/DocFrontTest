e3c9d42ab08734ff92bb2e36fbeea7ba3be764e0
Giampaolo1 <55143439+Giampaolo1@users.noreply.github.com>
2020-05-29 15:22:59 +0200

    a

--- carousel.js
+++ carousel.js
@@ -16,49 +16,91 @@ var options1 = {
   }
 }
 
-options1.fetchCards();
-console.log(options1.container);
+var options2 = {
+  container: "my-carousel",
+  title : "Another carousel instance title",
+  subtitle: "English Text Example",
+  fetchCards: function() {
+  }
+}
 
-document.getElementById('title').innerHTML=options1.title;
-document.getElementById('subtitle').innerHTML=options1.subtitle;
+
+
+
+
+
+
+
+
+
 
 $( document ).ready(function() {
-
   // Scelgo una freccia (next)
 
-  $(".next").click(nextSlide);
-    // console.log("cliccato");
+  // $(".next").click(nextSlide);
+  //   // console.log("cliccato");
+  // $(".prev").click(previousSlide)
 
-  $(".prev").click(previousSlide)
+  $(".next").click(function(){
+     $(".card2").show(1000);
+     $(".prev").show(1000);
 
+     $(".card").hide(1000);
+     $(".next").hide();
+
+
+     options2.fetchCards();
+     document.getElementById('title').innerHTML=options2.title;
+     document.getElementById('subtitle').innerHTML=options2.subtitle;
+
+     document.getElementById('imgtop1').src="https://source.unsplash.com/150x60/?brand";
+     document.getElementById('imgtop2').src="https://source.unsplash.com/150x60/?advert";
+
+   });
+
+   $(".prev").click(function(){
+     $(".card2").hide(1000);
+     $(".prev").hide();
+
+
+     $(".card").show(1000);
+     $(".next").show();
+
+
+     options1.fetchCards();
+     // console.log(options1.container);
+     document.getElementById('title').innerHTML=options1.title;
+     document.getElementById('subtitle').innerHTML=options1.subtitle;
+
+   });
 
 });
 
-function nextSlide (){
-
-  var posizioneimg = $(".card.active");
-  posizioneimg.removeClass("active");
-
-  if (posizioneimg.hasClass("last")) {
-  $(".card.first").addClass("active");
-  } else {
-    // Altrimenti passa l active alla prossima
-    posizioneimg.next(".card").addClass("active");
-  }
-}
-
-function previousSlide (){
-
-  var posizioneimg = $(".card.active");
-  posizioneimg.removeClass("active");
-
-  if (posizioneimg.hasClass("first")) {
-  $(".card.first").addClass("active");
-  } else {
-    // Altrimenti passa l active alla precedente
-    posizioneimg.prev(".card").addClass("active");
-  }
-}
+// function nextSlide (){
+//
+//   var posizioneimg = $(".card.active");
+//   posizioneimg.removeClass("active");
+//
+//   if (posizioneimg.hasClass("last")) {
+//   $(".card.first").addClass("active");
+//   } else {
+//     // Altrimenti passa l active alla prossima
+//     posizioneimg.next(".card").addClass("active");
+//   }
+// }
+//
+// function previousSlide (){
+//
+//   var posizioneimg = $(".card.active");
+//   posizioneimg.removeClass("active");
+//
+//   if (posizioneimg.hasClass("first")) {
+//   $(".card.first").addClass("active");
+//   } else {
+//     // Altrimenti passa l active alla precedente
+//     posizioneimg.prev(".card").addClass("active");
+//   }
+// }
 
 $(".containerGlobale").mouseover(function () {
     $(".next, .prev").addClass("opac")
